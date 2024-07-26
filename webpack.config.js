a054a9238ec5bd47b6d61c0c119df4bea1977475
FC-HaidarAli <169917859+FC-HaidarAli@users.noreply.github.com>
2024-07-26 09:54:12 -0400

    inital commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Restaurant Page',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
